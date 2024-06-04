852fddf1dcf76c94612a4ca7153725fa34840599
Jinje Han <coffeetea99iro@gmail.com>
2024-06-04 14:18:56 +0900

    replace ex2 main.ml

--- 2024/hw8/ex2/main.ml
+++ 2024/hw8/ex2/main.ml
@@ -1,27 +1,15 @@
 (*
  * SNU 4190.310 Programming Languages 
- * Main driver of homework "Rozetta" 
- * Dongkwon Lee (dklee@ropas.snu.ac.kr)
+ * Main driver of homework "RozettaX" 
  *)
 
-(* For OCaml version < 4.02, use the following function *)
-(*
-let read_all filename = 
-  let chan = open_in filename in 
-  let len = in_channel_length chan in
-  let buf = String.create len in
-  let _ = really_input chan buf 0 len in
-  let _ = close_in chan in 
-  buf
-*)
-
-let read_all filename = 
-  let chan = open_in filename in 
+let read_all filename =
+  let chan = open_in filename in
   let res = really_input_string chan (in_channel_length chan) in
-  let _ = close_in chan in 
+  let _ = close_in chan in
   res
 
-let main () =
+let () =
   let psonata = ref false in
   let sm5 = ref false in
   let src = ref "" in
@@ -29,9 +17,10 @@ let main () =
   let filename = Filename.basename Sys.argv.(0) in
   let _ =
     Arg.parse
-      [ ("-psonata", Arg.Set psonata, "print translated sonata code");
+      [
+        ("-psonata", Arg.Set psonata, "print translated sonata code");
         ("-sm5", Arg.Set sm5, "run with sm5 interpreter");
-        ("-debug", Arg.Set debug, "prints machine state every step")
+        ("-debug", Arg.Set debug, "prints machine state every step");
       ]
       (fun x -> src := x)
       ("Usage: " ^ filename ^ " [-psonata | -sm5] [-debug] [file]")
@@ -42,8 +31,7 @@ let main () =
 
   let pgm_str = read_all !src in
   let pgm = Parser.parse_sm5 pgm_str in
-  if !psonata then print_endline (Sonata.command_to_str "" (Rozetta.trans pgm))
-  else if !sm5 then Sm5.run pgm 
-  else Sonata.run (Rozetta.trans pgm)
-
-let _ = main ()
+  if !psonata then
+    print_endline (Sonata.command_to_str "" (Translator.trans pgm))
+  else if !sm5 then Sm5.run pgm
+  else Sonata.run (Translator.trans pgm)
